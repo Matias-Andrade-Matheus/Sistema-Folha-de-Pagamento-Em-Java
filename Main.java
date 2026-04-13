7d732ac5775f4847942f55326181a375b2f8d539
rafaalves0 <rafaelpinaalves00@gmail.com>
2026-04-13 20:36:12 -0300

    removendo teste

--- Main.java
+++ Main.java
@@ -4,7 +4,4 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
-};;;
-
-
-// não sei se tô fazendo certo, mas acho q tô começando a entender
+}
