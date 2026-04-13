6d40923ad066b9b82f64f3ec24507784260419d3
Matias-Andrade-Matheus <202318360042@ifba.edu.br>
2026-04-13 20:37:05 -0300

    teste de pull e push 2(mat)

--- Main.java
+++ Main.java
@@ -4,7 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+<<<<<<< HEAD
 };;;
 
 
 // não sei se tô fazendo certo, mas acho q tô começando a entender
+// tá mesmo testa agoraaaaaaaaaa
+=======
+}
+>>>>>>> 7d732ac5775f4847942f55326181a375b2f8d539
